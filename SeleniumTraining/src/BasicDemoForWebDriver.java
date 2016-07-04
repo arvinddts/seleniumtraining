24a25ea56fb386e90c0ace92908b4aae0086a266
arvind <arvind@arvind-PC>
2016-07-05 00:00:26 +0530

    changes to basicdemoforwebdriver
    Change-Id: Ib88437f5babaf8aad6e51431f386fa73f12aaa09

--- SeleniumTraining/src/BasicDemoForWebDriver.java
+++ SeleniumTraining/src/BasicDemoForWebDriver.java
@@ -20,7 +20,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasicDemoForWebDriver {
+public class BasicDemoForWebDriver
+{
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "/Users/arvind/Downloads/chromedriver");
